8928e53f2774a6748664160e50803db782e777e6
nekosu <liao.hy@outlook.com>
2023-07-11 20:20:11 +0800

    feat: build a minify ver

--- res/build.ts
+++ res/build.ts
@@ -12,6 +12,24 @@ async function build_pipeline() {
     const obj = $$()
     await mkdir('dist', { recursive: true })
     await writeFile('dist/pipeline.json', JSON.stringify(obj, null, 2))
+    await writeFile(
+      'dist/pipeline.min.json',
+      JSON.stringify(obj, (key, value) => {
+        if (value instanceof Array) {
+          return value
+        } else if (typeof value === 'object') {
+          const res = {}
+          for (const k in value) {
+            if (!k.endsWith('@meta')) {
+              res[k] = value[k]
+            }
+          }
+          return res
+        } else {
+          return value
+        }
+      })
+    )
   } catch (err) {
     console.error(err)
   }
